5c10222a316eac049b462ccbbacaa03e4d51f6fe
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-08 23:21:49 +0000

    Fri, Feb 9, 2024, 12:21 AM +01:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,3 +1,4 @@
+
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
@@ -5,16 +6,19 @@ def main():
     print(f"Leave ${tip:.2f}")
 
 
-def dollars_to_float(d):
+def dollars_to_float(amount_str):
+    # Remove the leading $
     amount_str = amount_str.lstrip('$')
     # Convert the string to a float
     return float(amount_str)
 
 
-def percent_to_float(p):
+def percent_to_float(percentage_str):
+    # Remove the trailing %
     percentage_str = percentage_str.rstrip('%')
     # Convert the string to a float and divide by 100 to get the decimal representation of the percentage
     return float(percentage_str) / 100
 
 
 main()
+
