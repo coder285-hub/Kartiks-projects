f0b892ec54fc47381a5d297e52d3e87c13d689c3
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-09 23:18:28 +0000

    Sat, Feb 10, 2024, 12:18 AM +01:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,13 +1,12 @@
 expression = input("Enter an arithmetic expression (e.g., 1 + 1): ")
 
-
 z, y, x = expression.split()
 
-    # Convert operands to integers
+# Convert operands to integers
 x = int(x)
 z = int(z)
 
-    # Perform the operation based on the operator
+# Perform the operation based on the operator
 if y == '+':
     result = z + x
 elif y == '-':
@@ -17,9 +16,10 @@ elif y == '*':
 elif y == '/':
     try:
         result = z / x
-    except (ValueError, ZeroDivisionError):
+    except ZeroDivisionError:
         print("Invalid input. Please enter a valid arithmetic expression.")
+        exit()
 
-print(round(result,1))
+print(round(result, 1))
 
 
