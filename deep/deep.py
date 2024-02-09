f59a374a8daa8417502b54edc50d7abf7897be9c
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-09 14:53:11 +0000

    Fri, Feb 9, 2024, 3:53 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,6 @@
 user_input=input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
 
-if user_input=="42" or user_input=="forty-two" or user_input=="Forty two":
+if user_input==" 42 " or user_input=="forty-two" or user_input=="Forty two" or user_input=="forty two" or user_input=="FoRty TwO":
     print("yes")
 
 else:
