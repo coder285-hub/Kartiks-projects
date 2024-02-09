c2a8fc4367d781342b8c428114f359abeff54153
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-09 15:16:06 +0000

    Fri, Feb 9, 2024, 4:16 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,8 @@
 user_input=input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
 
-if user_input=="42" or user_input==" 42" or user_input =="42 "or user_input=="forty-two" or user_input=="Forty two" or user_input=="forty two" or user_input=="FoRty TwO":
+user_input_stripped = user_input.strip()
+
+if user_input_stripped == "42" or user_input_stripped == "forty-two" or user_input_stripped == "Forty two" or user_input_stripped == "forty two" or user_input_stripped == "FoRty TwO":
     print("yes")
 
 else:
