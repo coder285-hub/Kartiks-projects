4b7b2a166343f0f73456713e49364926ae07916d
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-09 22:29:04 +0000

    Fri, Feb 9, 2024, 11:29 PM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,4 +1,5 @@
 file_name = input("Enter the name of the file: ")
-file_extension=file_name.rsplit(".",1)
+file_extension=file_name.rsplit(".",1)[1]
 if file_extension.lower() in ['gif', 'jpg', 'jpeg', 'png']:
-    
+    print("image/",file_extension)
+
