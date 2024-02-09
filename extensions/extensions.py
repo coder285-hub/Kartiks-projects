3100fd8e266bd9e52ae080fbffe8045cf0966b56
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-09 22:42:02 +0000

    Fri, Feb 9, 2024, 11:42 PM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,9 +1,9 @@
 file_name = input("Enter the name of the file: ")
 file_extension=file_name.rsplit(".",1)[1]
 if file_extension.lower() in ['gif', 'jpg', 'jpeg', 'png']:
-    print("image/",file_extension)
+    print(f"image/{file_extension}")
 elif file_extension.lower() in ['pdf', 'txt', 'zip']:
-    print("application/",file_extension)
+    print(f"application/{file_extension}")
 else:
     print("application/octet-stream")
 
