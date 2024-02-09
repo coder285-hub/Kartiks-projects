ce62ba35cf4d5f6580ae15c3eb8f09156737ba36
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-09 16:11:36 +0000

    Fri, Feb 9, 2024, 5:11 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,1 +1,8 @@
-balances ={"Hello":0,"Hello, Newman":0,"How you doing":}
+balances ={"Hello":0,"Hello, Newman":0,"How you doing":20,"What's happening?":100}
+
+user_input=input()
+
+if user_input in balances:
+    print("$"+balances[user_input])
+else:
+    print("Invalid Input")
