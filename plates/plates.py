9facfc177e910b212febc3177a9c281ba2740cb6
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-13 00:16:43 +0000

    Tue, Feb 13, 2024, 1:16 AM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,23 +10,19 @@ def is_valid(plate):
 
     if not plate[:2].isalpha():
         return False
-
+    
     # Rule 2: Plate must have a minimum of 2 characters and a maximum of 6 characters
     if not 2 <= len(plate) <= 6:
         return False
-
-    # Rule 3: Numbers can only appear at the end of the plate
-    if any(c.isdigit() for c in plate[:-1]):
-        return False
-
-    # Rule 4: The first number used cannot be '0'
+    
+    # Rule 3: The first number used cannot be '0'
     if plate[0].isdigit() and plate[0] == '0':
         return False
-
-    # Rule 5: No periods, spaces, or punctuation marks are allowed
+    
+    # Rule 4: No periods, spaces, or punctuation marks are allowed
     if any(not c.isalnum() for c in plate):
         return False
-
+    
     return True
 
 
