83678ac3069e20e89879d5a66f8703a29157d052
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-13 00:25:11 +0000

    Tue, Feb 13, 2024, 1:25 AM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,19 +10,22 @@ def is_valid(plate):
 
     if not plate[:2].isalpha():
         return False
-    
+
     # Rule 2: Plate must have a minimum of 2 characters and a maximum of 6 characters
     if not 2 <= len(plate) <= 6:
         return False
-    
+
     # Rule 3: The first number used cannot be '0'
     if plate[0].isdigit() and plate[0] == '0':
         return False
-    
+
     # Rule 4: No periods, spaces, or punctuation marks are allowed
     if any(not c.isalnum() for c in plate):
         return False
     
+    if any(c.isdigit() for c in plate[:-1]):
+        return False
+
     return True
 
 
