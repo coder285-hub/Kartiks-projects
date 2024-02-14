f6a70d0878132ed9db6be655a5887e4f2cf1687e
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-14 23:59:09 +0000

    Thu, Feb 15, 2024, 12:59 AM +01:00

--- game/game.py
+++ game/game.py
@@ -2,4 +2,16 @@ import random
 
 while True:
     level = int(input("Level:" ))
-    
+    while True:
+            guess = int(input("Guess: "))
+            if guess > x:
+                print("Too large!")
+            elif guess < x:
+                print("Too small!")
+            elif guess == x:
+                print("Just right!")
+                raise EOFError
+    except ValueError:
+        pass
+    except EOFError:
+        break
