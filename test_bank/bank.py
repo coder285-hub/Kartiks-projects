edaa1d3ea764c42d56e20acdd1cd9c6352711440
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-15 21:04:57 +0000

    Thu, Feb 15, 2024, 10:04 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,9 +1,18 @@
 def main():
-    
+
 
 
 def value(greeting):
-    ...
+    balances ={"Hello":0," Hello ":0,"Hello, Newman":0,"How you doing?":20,"What's happening?":100,"What's up?":100}
+
+    user_input=input("Greeting: ")
+
+    if user_input in balances:
+        print("$"+str(balances[user_input]))
+
+    else:
+        print("Invalid Input")
+
 
 
 if __name__ == "__main__":
