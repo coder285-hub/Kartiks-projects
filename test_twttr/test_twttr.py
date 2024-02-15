085ead692f3a6a1fb64eca29f4fa644f5573951d
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-15 20:05:05 +0000

    Thu, Feb 15, 2024, 9:05 PM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,12 +1,6 @@
 from twttr import shorten
-def main():
-     lower_uppercase_test()
-# Call test functions test_upper_lower_cases()
-# Test letters upper and lower cases def test_upper_lower_cases(): ...assert shorten('twitter')
-def lower_uppercase_test():
-     assert shorten('TWITTER') == 'TWTTR'
-     assert shorten('twitter') == 'twttr'
-# Test numbers
-
-if __name__ == "__main__":
-     main()
+def test_assert():
+    assert shorten("hello world") == "hll wrld"
+    assert shorten("HELLO WORLD") == "HLL WRLD"
+    assert shorten("h3ll0 w0rld") == "h3ll0 w0rld"
+    assert shorten("h@llo w*rld!!!") == "h@ll w*rld!!!"
