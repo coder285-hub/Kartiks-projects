183f6b7842f1dfe64c5629350ff3533220f70ee9
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-15 19:52:48 +0000

    Thu, Feb 15, 2024, 8:52 PM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -4,7 +4,9 @@ def main():
     lower_uppercase_test()
 
 def lower_uppercase_test():
+    # Test with lowercase input
     assert shorten("twitter") == "twttr"
+    # Test with uppercase input
     assert shorten("TWITTER") == "TWTTR"
 
 if __name__ == "__main__":
