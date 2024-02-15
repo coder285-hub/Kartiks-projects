da4b775c355cf17ae75bcc2c15928239fca50b45
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-15 19:38:30 +0000

    Thu, Feb 15, 2024, 8:38 PM +01:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -7,7 +7,7 @@ def shorten(word):
     vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
     result = ""
 
-    for char in user_input:
+    for char in word:
         if char not in vowels:
             result = result + char
 
