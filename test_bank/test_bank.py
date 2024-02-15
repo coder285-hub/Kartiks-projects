d5da7699729e383c0c91826dc2892df5b2c2d2f1
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-15 22:24:47 +0000

    Thu, Feb 15, 2024, 11:24 PM +01:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -4,13 +4,12 @@ def main():
     test_lowercase()
 
 def test_lowercase():
-    assert value ("hi world")==20
-    assert value("hello world") == 0
-    assert value("HELLO WORLD") == 0
-    assert value("HI WORLD") == 20
-    assert value("wassup world") == 100
-    assert value("WASSUP WORLD") == 100
-
+    assert value("hi world") == "$20"
+    assert value("hello world") == "$0"
+    assert value("HELLO WORLD") == "$0"
+    assert value("HI WORLD") == "$20"
+    assert value("wassup world") == "$100"
+    assert value("WASSUP WORLD") == "$100"
 
 if __name__ == "__main__":
     main()
