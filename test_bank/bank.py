9ad6b751c4879d20a5b7229f95299ab612de3aae
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-02-17 15:16:33 +0000

    Sat, Feb 17, 2024, 4:16 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,26 +1,15 @@
 def main():
-    greeting_input = input("Greeting: ")
-    print(f"${calculate_balance(greeting_input)}")
+    greet = (input("Greeting: ")).lower().strip()
+    print(f"${value(greet)}")
 
-def calculate_balance(greeting):
-    balances = {
-        "hello": 0,
-        "hi": 20,
-    }
 
-    total_balance = 0
-
-    words = greeting.lower().strip().split()  # Split the greeting into words
-
-    for word in words:
-        if word[0].lower()=="hi": # Convert the word to lowercase before checking
-            total_balance += 20  # Add the balance for "hi" to the total balance
-        elif word[0].lower()=="hello":
-            total_balance += 0  # Add the balance for "hello" to the total balance
-        else:
-            total_balance += 100  # Add the default balance to the total balance
-
-    return total_balance
+def value(greeting):
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
+    else:
+        return 100
 
 if __name__ == "__main__":
     main()
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,17 +1,12 @@
 from bank import value
 
-def main():
-    test_lowercase()
 
-def test_lowercase():
-    assert value("hi world") == "$20"
-    assert value("HI WORLD") == "$20"
-    assert value("hello world") == "$0"
-    assert value("HELLO WORLD") == "$0"
-    assert value("wassup world") == "$100"
-    assert value("WASSUP WORLD") == "$100"
-
-if __name__ == "__main__":
-    main()
+def test_greeting():
+    assert value("hello world") == 0
+    assert value("HELLO WORLD") == 0
+    assert value("hi world") == 20
+    assert value("HI WORLD") == 20
+    assert value("wassup world") == 100
+    assert value("WASSUP WORLD") == 100
 
 
