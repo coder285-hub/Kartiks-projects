12a09a46eb31caaefe5330ef924eb5b8bec69621
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-07 21:17:18 +0000

    Thu, Mar 7, 2024, 10:17 PM +01:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,6 +1,14 @@
 
 -- *** The Lost Letter ***
+SELECT id FROM addresses
+WHERE address LIKE "2 Fin%"
 
+SELECT * FROM packages
+O WHERE from_address_id = (
+    SELECT id FROM addresses -SELECT-id-FROM-addresses
+    WHERE address = "900 Somerville Avenue"
+)AND to_address_id = (
+);
 
 
 -- *** The Devious Delivery ***
