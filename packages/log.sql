95b35ee1255af61e30d82a2c8fb090fdbf6e6f73
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-09 22:08:14 +0000

    Sat, Mar 9, 2024, 11:08 PM +01:00

--- packages/log.sql
+++ packages/log.sql
@@ -32,3 +32,20 @@ WHERE from_address_id(
 
 );
 
+SELECT * FROM "drivers"
+WHERE "id" = (
+    SELECT "driver_id" FROM "scans"
+    WHERE "package_id" = (
+        SELECT "id" FROM "packages"
+        WHERE "from_address_id" = (
+            SELECT "id" FROM "addresses"
+            WHERE "address" = '109 Tileston Street'
+        ) AND "to_address_id" = (
+            SELECT "id" FROM "addresses"
+            WHERE "address" = '728 Maple Place'
+    )
+    )
+    ORDER BY "timestamp" DESC
+    LIMIT 1
+);
+
