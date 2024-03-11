ffaff4a9d7f9a8b242829a3393d5705ca88e8e2d
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-11 20:14:37 +0000

    Mon, Mar 11, 2024, 9:14 PM +01:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,8 +1,17 @@
-SELECT districts.name, expenditures.per_pupil_expenditure,staff_evaluations.exemplary FROM districts
-JOIN expenditures ON expenditures.district_id = districts.id
-JOIN staff_evaluations ON staff_evaluations.district_id = districts.id
-WHERE districts.type ="Public School District" AND per_pupil_expenditure >=(
-    SELECT AVG(per_pupil_expenditure) FROM expenditures
-)AND exemplary >=(
-    SELECT AVG(exemplary) FROM staff_evaluations
-);
+SELECT "name", "per_pupil_expenditure", "exemplary" FROM "districts"
+JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
+JOIN "staff_evaluations" ON "staff_evaluations"."district_id" = "districts"."id"
+WHERE "name" IN (
+    SELECT "name" FROM "districts"
+    WHERE "id" IN (
+        SELECT DISTINCT("district_id") FROM "schools"
+        WHERE "type" = 'Public School'
+    )
+)
+AND "per_pupil_expenditure" > (
+    SELECT AVG("per_pupil_expenditure") FROM "expenditures"
+)
+AND "exemplary" > (
+    SELECT AVG("exemplary") FROM "staff_evaluations"
+)
+ORDER BY "exemplary" DESC, "per_pupil_expenditure" DESC;
