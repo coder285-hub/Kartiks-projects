de2a90b5e35cb963c1d00193a7723b55dd949c2a
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-11 20:13:57 +0000

    Mon, Mar 11, 2024, 9:13 PM +01:00

--- dese/10.sql
+++ dese/10.sql
@@ -1,5 +1,5 @@
 SELECT name, per_pupil_expenditure
 FROM districts
-JOIN expenditures ON districts.id=expenditures.district_id
+JOIN expenditures ON expenditures.district_id=districts.id
 WHERE type LIKE "%Public School%"
 ORDER BY per_pupil_expenditure DESC;
