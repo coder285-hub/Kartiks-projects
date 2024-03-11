a65aeee1274e346936cfb29f675859128c01c5b9
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-11 19:59:28 +0000

    Mon, Mar 11, 2024, 8:59 PM +01:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,2 +1,4 @@
 SELECT districts.name, expenditures.per_pupil_expenditure,staff_evaluations.exemplary FROM districts
-JOIN expenditures ON district.id
+JOIN expenditures ON district.id = expenditures.district_id
+JOIN staff_evaluations ON staff_evaluation.district_id = district_id
+WHERE districts.type ="Public School District" 
