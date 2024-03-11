1019380af5f31f89417328f159e2d51e4ebcaaab
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-11 22:31:25 +0000

    Mon, Mar 11, 2024, 11:31 PM +01:00

--- moneyball/8.sql
+++ moneyball/8.sql
@@ -2,7 +2,7 @@ SELECT salary FROM salaries
 WHERE player_id =(
     SELECT player_id FROM performances
     WHERE "HR" =(
-        SELECT MAX(HR) FROM performances
+        SELECT MAX("HR") FROM performances
         WHERE year ="2001"
     )
 );
