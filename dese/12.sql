ad8429baed92361e1de65bcefb1d9e4344f3b6fd
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-11 20:06:47 +0000

    Mon, Mar 11, 2024, 9:06 PM +01:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,5 +1,5 @@
 SELECT districts.name, expenditures.per_pupil_expenditure,staff_evaluations.exemplary FROM districts
-JOIN expenditures ON districts.id = expenditures.district_id
+JOIN expenditures ON expenditures.district_id + district_id
 JOIN staff_evaluations ON staff_evaluations.district_id = district_id
 WHERE districts.type ="Public School District" AND per_pupil_expenditure >=(
     SELECT AVG(per_pupil_expenditure) FROM expenditures
