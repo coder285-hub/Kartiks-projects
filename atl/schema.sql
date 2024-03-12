8fcbe6fdff6166e87fe733833159adea79933f11
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-12 20:26:35 +0000

    Tue, Mar 12, 2024, 9:26 PM +01:00

--- atl/schema.sql
+++ atl/schema.sql
@@ -1,39 +1,36 @@
-CREATE TABLE Passengers (
-    "id" INTEGER
-    "first_name" TEXT
-    "last_name" TEXT
-    "age" INTEGER
-    PRIMARY KEY("id")
-
-);
-
-CREATE TABLE Airlines(
+CREATE TABLE "passengers" (
     "id" INTEGER,
-    "name" TEXT,
-    "concourse" TEXT,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL CHECK("age" > 0),
     PRIMARY KEY("id")
 );
 
-
-CREATE TABLE Flights(
+CREATE TABLE "airlines" (
     "id" INTEGER,
-    "flight_number" INTEGER,
-    "airline_id"-INTEGER,
-    "departing_airport_code" TEXT, "arrival_airport_code" TEXT,
-    "departing_datetime" DATETIME,
-    "arrival_datetime", DATETIME,
+    "name" TEXT NOT NULL UNIQUE,
+    "concourse" TEXT NOT NULL CHECK("concourse" IN ('A', 'B', 'C', 'D', 'E', 'F', 'T')),
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "flights" (
+    "id" INTEGER,
+    "number" INTEGER NOT NULL,
+    "airline_id" INTEGER,
+    "from_airport_code" TEXT NOT NULL CHECK("from_airport_code" != "to_airport_code"),
+    "to_airport_code" TEXT NOT NULL CHECK("to_airport_code" != "from_airport_code"),
+    "departure_time" NUMERIC NOT NULL CHECK("departure_time" < "arrival_time"),
+    "arrival_time" NUMERIC NOT NULL CHECK("arrival_time" > "departure_time"),
     PRIMARY KEY("id"),
-    FOREIGN KEY ("airline_id") REFERENCES Airlines("id")
+    FOREIGN KEY("airline_id") REFERENCES "airlines"("id")
 );
 
-CREATE TABLE Check-Ins (
+CREATE TABLE "check_ins" (
     "id" INTEGER,
-    "datetime" DATETIME,
-    "flight_id" INTEGER,
     "passenger_id" INTEGER,
+    "flight_id" INTEGER,
+    "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY("id"),
-    FOREIGN KEY ("flight_id") REFERENCES Airlines("id"),
-    FOREIGN KEY ("passenger_id") REFERENCES Passengers("id")
-    );
-
-
+    FOREIGN KEY("passenger_id") REFERENCES "passengers"("id"),
+    FOREIGN KEY("flight_id") REFERENCES "flights"("id")
+);
