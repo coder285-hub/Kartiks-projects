8a635f107b61f9180c9fae949ad2c8ab2cf2a4ef
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-12 22:15:30 +0000

    Tue, Mar 12, 2024, 11:15 PM +01:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
-    "username" TEXT NOT NULL UNIQUE
-    "password" TEXT NOT NULL
+    "username" TEXT NOT NULL UNIQUE,
+    "password" TEXT NOT NULL,
     Primary Key ("id")
 );
 
