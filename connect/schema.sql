d44d26e5c02a27af8ba60666a96c13fc6234c531
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-12 22:35:21 +0000

    Tue, Mar 12, 2024, 11:35 PM +01:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -1,38 +1,40 @@
-CREATE TABLE users (
+CREATE TABLE "users" (
+    "id" INTEGER,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
     "username" TEXT NOT NULL UNIQUE,
     "password" TEXT NOT NULL,
-    Primary Key("id")
+    PRIMARY KEY("id")
 );
 
-CREATE TABLE Schools (
-    "id" INTEGER NOT NULL,
+CREATE TABLE "schools" (
+    "id" INTEGER,
     "name" TEXT NOT NULL,
     "type" TEXT,
     "location" TEXT,
     "year_founded" NUMERIC,
-    Primary Key("id")
+    PRIMARY KEY("id")
 );
 
-CREATE TABLE Companies (
-    "id" INTEGER NOT NULL,
+CREATE TABLE "companies" (
+    "id" INTEGER,
     "name" TEXT NOT NULL UNIQUE,
-    "Industry" TEXT NOT NULL,
-    "Location" TEXT NOT NULL,
+    "industry" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    PRIMARY KEY("id")
 );
 
-CREATE TABLE people_connections (
+CREATE TABLE "people_connections" (
     "id" INTEGER,
     "user_id" INTEGER,
     "following_id" INTEGER,
     PRIMARY KEY("id"),
-    FOREIGN KEY("user_id") REFERENCES users("id"),
-    FOREIGN KEY("following_id") REFERENCES users("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id"),
+    FOREIGN KEY("following_id") REFERENCES "users"("id"),
     CONSTRAINT unique_connection UNIQUE (user_id, following_id)
 );
 
-CREATE TABLE school_connections (
+CREATE TABLE "school_connections" (
     "id" INTEGER,
     "user_id" INTEGER,
     "school_id" INTEGER,
@@ -40,21 +42,22 @@ CREATE TABLE school_connections (
     "end_date" DATE,
     "degree_type" TEXT,
     PRIMARY KEY("id"),
-    FOREIGN KEY("user_id") REFERENCES users("id"),
-    FOREIGN KEY("school_id") REFERENCES Schools("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id"),
+    FOREIGN KEY("school_id") REFERENCES "schools"("id"),
     CONSTRAINT unique_school_connection UNIQUE (user_id, school_id)
 );
-CREATE TABLE company_connections (
+
+CREATE TABLE "company_connections" (
     "id" INTEGER,
     "user_id" INTEGER,
     "company_id" INTEGER,
     "start_date" DATE,
     "end_date" DATE,
-    "company_type" TEXT,
+    "title" TEXT,
     PRIMARY KEY("id"),
-    FOREIGN KEY("user_id") REFERENCES users ("id"),
-    FOREIGN KEY("company_id") REFERENCES Companies ("id"),
-    CONSTRAINT unique_comapny_connection UNIQUE (user_id, company_id)
+    FOREIGN KEY("user_id") REFERENCES "users"("id"),
+    FOREIGN KEY("company_id") REFERENCES "companies"("id"),
+    CONSTRAINT unique_company_connection UNIQUE (user_id, company_id)
 );
 
 
