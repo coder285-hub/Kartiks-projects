0781ee527ed54bdad50014156dd7df91a4d26ec6
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-13 20:08:13 +0000

    Wed, Mar 13, 2024, 9:08 PM +01:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,9 +1,20 @@
 .import --csv meteorites.csv meteorites_temp
 
 UPDATE meteorites_temp
-SET "mass" =NULL
-WHERE id =(
-    SELECT id FROM meteorites_temp
-    WHERE mass =""
+SET mass ="NULL"
+where mass =0;
 
-);
+UPDATE meteorites_temp
+SET year ="NULL"
+where year =0;
+
+UPDATE meteorites_temp
+SET lat ="NULL"
+where lat =0;
+
+UPDATE meteorites_temp
+SET long ="NULL"
+where long =0;
+
+
+
