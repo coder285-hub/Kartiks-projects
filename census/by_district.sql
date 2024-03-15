9f8b4a3ffa5bdda09aef0e9a1fea1b13f851502b
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-15 20:43:55 +0000

    Fri, Mar 15, 2024, 9:43 PM +01:00

--- census/by_district.sql
+++ census/by_district.sql
@@ -1,3 +1,10 @@
-CREATE VIEW "rural" AS
-SELECT * FROM "census"
-WHERE "locality" LIKE '%Rural%';
+CREATE VIEW "by_district" AS
+SELECT
+    "district",
+    SUM("families") AS "families",
+    SUM("households") AS "households",
+    SUM("population") AS "population",
+    SUM("male") AS "male",
+    SUM("female") AS "female"
+FROM "census"
+GROUP BY "district";
