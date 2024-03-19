c6faa14e1976589f47d1f2384316f75be2e9e5f0
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-19 18:15:30 +0000

    Tue, Mar 19, 2024, 7:15 PM +01:00

--- snap/4.sql
+++ snap/4.sql
@@ -1,9 +1,9 @@
-SELECT users.username
+SELECT u.username
 FROM users u
 JOIN (
     SELECT to_user_id, COUNT(*) AS message_count
     FROM messages
     GROUP BY to_user_id
-) AS message_counts ON users.id = message_counts.to_user_id
+) AS message_counts ON u.id = message_counts.to_user_id
 ORDER BY message_counts.message_count DESC, u.username ASC
 LIMIT 1;
