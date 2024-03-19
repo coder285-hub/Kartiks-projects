056ee3800a74e32024b526879d3d6c5cb7c086d4
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-19 18:07:40 +0000

    Tue, Mar 19, 2024, 7:07 PM +01:00

--- snap/4.sql
+++ snap/4.sql
@@ -1,5 +1,9 @@
-SELECT "users"."username" FROM "users"
-JOIN "messages" ON "users"."id" = "messages"."from_user_id"
-GROUP BY ("to_user_id")
-ORDER BY COUNT("messages"."from_user_id") DESC,"users"."username"
-limit 1;
+SELECT users.username
+FROM users u
+JOIN (
+    SELECT to_user_id, COUNT(*) AS message_count
+    FROM messages
+    GROUP BY to_user_id
+) AS message_counts ON users.id = message_counts.to_user_id
+ORDER BY message_counts.message_count DESC, u.username ASC
+LIMIT 1;
