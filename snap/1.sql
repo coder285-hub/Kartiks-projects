614b3165cbb46bd613c849fff73bd0d0284e3037
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-19 15:21:20 +0000

    Tue, Mar 19, 2024, 4:21 PM +01:00

--- snap/1.sql
+++ snap/1.sql
@@ -1,2 +1,2 @@
 SELECT "username" FROM "users"
-WHERE "last_login_date >
+WHERE "last_login_date" > '2024-01-01';
---
+++ snap/3.sql
@@ -0,0 +1,5 @@
+SELECT "to_user_id" FROM "messages"
+WHERE "from_user_id" = (
+    SELECT "id" FROM "users" WHERE "username" = 'creativewisdom377'
+    
+)
