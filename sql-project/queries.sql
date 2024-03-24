a2386cccf42b2b8381431f53c95e5cf8439ad4c7
coder285-hub <159490247+coder285-hub@users.noreply.github.com>
2024-03-24 12:39:45 +0000

    Sun, Mar 24, 2024, 1:39 PM +01:00

--- sql-project/queries.sql
+++ sql-project/queries.sql
@@ -3,3 +3,7 @@ SELECT * FROM Books WHERE author_id =1;
 SELECT * FROM Orders WHERE customer_id =1;
 
 INSERT INTO Inventory (book_id , quantity) VALUES (1,100);
+
+UPDATE Inventory SET quantity = 80 WHERE book_id = 1;
+
+DELETE FROM Orders WHERE order_id = 1;
